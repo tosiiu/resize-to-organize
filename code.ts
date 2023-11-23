905221292ba9a973e3ed39c64f028bebdc36cdd7
tosiiu <48958782+tosiiu@users.noreply.github.com>
2023-11-23 23:47:04 +0900

    コマンドの変更に合わせて処理を追加/若干コード整理

--- code.ts
+++ code.ts
@@ -1,58 +1,77 @@
-function findMaxSize(){
+function findMaxSize() {
   let maxWidth = 0;
   let maxHeight = 0;
   for (const node of figma.currentPage.selection) {
-    if(node.width > maxWidth){
+    if (node.width > maxWidth) {
       maxWidth = node.width;
-    };
-    if(node.height > maxHeight){
+    }
+    if (node.height > maxHeight) {
       maxHeight = node.height;
     }
   }
-  return ([maxWidth, maxHeight]);
+  return [maxWidth, maxHeight];
 }
-function findMinSize(){
+function findMinSize() {
   let minWidth = figma.currentPage.selection[0].width;
   let minHeight = figma.currentPage.selection[0].height;
   for (const node of figma.currentPage.selection) {
-    if(node.width < minWidth){
+    if (node.width < minWidth) {
       minWidth = node.width;
-    };
-    if(node.height < minHeight){
+    }
+    if (node.height < minHeight) {
       minHeight = node.height;
     }
   }
-  return ([minWidth, minHeight]);
+  return [minWidth, minHeight];
 }
-function findStartPoint(){
+function findStartPoint() {
   let top = 0;
   let left = 0;
   for (const node of figma.currentPage.selection) {
     console.log(typeof node.x);
-    if(node.x < left){
+    if (node.x < left) {
       left = node.x;
-    };
-    if(node.y < top || top === 0){
+    }
+    if (node.y < top || top === 0) {
       top = node.y;
     }
   }
-  return ([left, top])
+  return [left, top];
 }
 const maxArray = findMaxSize();
 const minArray = findMinSize();
-// const frameSize = findStartPoint();
+
 for (const node of figma.currentPage.selection) {
-  console.log(node.type);
-  // node.resize(maxArray[0], maxArray[1]);
-  if(figma.command === 'min') {
-    node.resize(minArray[0], minArray[1]);
-  } else if (figma.command === 'max') {
-    node.resize(maxArray[0], maxArray[1]);
+  let width, height;
+
+  switch (figma.command) {
+    case "align-to-smallest":
+      [width, height] = minArray;
+      break;
+    case "align-to-biggest":
+      [width, height] = maxArray;
+      break;
+    case "align-to-widest":
+      width = maxArray[0];
+      height = node.height;
+      break;
+    case "align-to-narrowest":
+      width = minArray[0];
+      height = node.height;
+      break;
+    case "align-to-tallest":
+      width = node.width;
+      height = maxArray[1];
+      break;
+    case "align-to-shortest":
+      width = node.width;
+      height = minArray[1];
+      break;
+  }
+
+  if (width && height && "resize" in node) {
+    node.resize(width, height);
   }
 }
-// const frame = figma.createFrame();
-// frame.resize(579, 256);
-// frame.x = frameSize[0];
-// frame.y = frameSize[1];
-// node.layoutMode === 'VERTICAL'
-figma.closePlugin('Done');
+
+figma.closePlugin("Done");
